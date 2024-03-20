bd9bd275e8d090d93eb7839ea9061ad69f8829ee
Ricardo Andres Marín Pinilla <rmarin@novopayment.com>
2024-03-20 11:02:26 -0500

    Segundo Commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Richardev0258/GolangDesdeCero/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(123)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
